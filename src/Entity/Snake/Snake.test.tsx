a9a84d4d302e5d4b785c05d2576cf3f7809b992c
Jaakko Veikkolainen <jaakko.veikkolainen@houston-inc.com>
2018-09-30 13:17:22 +0300

    configure enzyme

---
+++ src/Entity/Snake/Snake.test.tsx
@@ -0,0 +1,30 @@
+import { Snake } from './Snake';
+
+describe('Snake', () => {
+  let entity: Snake;
+  let xStub: number;
+  let yStub: number;
+  let xSpeedStub: number;
+  let ySpeedStub: number;
+
+  beforeEach(() => {
+    xStub = 0;
+    yStub = 0;
+    xSpeedStub = 1;
+    ySpeedStub = 1;
+
+    entity = new Snake(xStub, yStub, xSpeedStub, ySpeedStub);
+  });
+
+  it('can move in x axis', () => {
+    entity.move();
+
+    expect(entity.x).toBe(xStub + xSpeedStub);
+  });
+
+  it('can move in y axis', () => {
+    entity.move();
+
+    expect(entity.y).toBe(yStub + ySpeedStub);
+  });
+});
--- src/canvas/Canvas.test.tsx
+++ src/canvas/Canvas.test.tsx
@@ -1,9 +1,10 @@
+import { mount } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
 import { Canvas } from './Canvas';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Canvas />, div);
+it('renders', () => {
+  const component = mount(<Canvas />);
+
+  expect(component).toMatchSnapshot();
 });
